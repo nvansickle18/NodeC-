11d4c1dea41eb30b656da6f63b43feaf30a9513c
Noah <nvansickle18@kentdenver.org>
2018-04-17 09:10:48 -0600

    Initial Commit

---
+++ NodeC++/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  NodeC++
+//
+//  Created by Noah VanSickle on 4/17/18.
+//  Copyright © 2018 Noah VanSickle. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
